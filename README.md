b7ca67d525e7a64db4e6e682b1f87c4e6dc8c67c
Tomoya Tanjo <ttanjo@gmail.com>
2017-10-17 17:59:23 +0900

    Add flycheck-cwl.el

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # flycheck-cwl
-Add on-the-fly checker for Common Workflow Language
+This library adds a on-the-fly syntax checker for [Common Workflow Language](http://www.commonwl.org) in Emacs by using [Flycheck](http://www.flycheck.org/).
+
+Requirements:
+  * Emacs 24.4 or later
+  * flycheck.el
+  * Schema Salad
+
+To use this package, add the following line to your .emacs file:
+```emacs
+    (require 'flycheck-cwl)
+    (setup-flycheck-cwl)
+    (setq flycheck-cwl-schema-path "/path/to/CommonWorkflowLanguage.yml")
+```
+It automatically enables on-the-fly syntax checker for CWL.
+
+KNOWN ISSUES:
+- The error messages are currently incomplete.
+- It shows redundant error messages.
+  - It will be fixed once [this request](https://github.com/common-workflow-language/schema_salad/pull/135) is merged.
+- This package cannot be installed via [MELPA](https://melpa.org/).
+  - Once the above issue is solved, I will do that.
---
+++ flycheck-cwl.el
@@ -0,0 +1,55 @@
+;;; Flycheck-cwl -- on-the-fly syntax checker for Common Workflow Language
+
+;; Version: 0.0.0
+;; Author: Tomoya Tanjo <ttanjo@gmail.com>
+;; URL: https://github.com/tom-tan/flycheck-cwl
+;; Package-Requires: ((flycheck "31"))
+;; Keywords: languages, flycheck, cwl, common workflow language
+
+;;; Commentary:
+
+;; This library adds a on-the-fly syntax checker for Common Workflow Language
+;; in Emacs by using Schema Salad.
+;;
+;; Requirements:
+;;   * Emacs 24.4 or later
+;;   * flycheck.el
+;;   * Schema Salad
+;;
+;; To use this package, add the following line to your .emacs file:
+;;     (require 'flycheck-cwl)
+;;     (setup-flycheck-cwl)
+;;     (setq flycheck-cwl-schema-path "/path/to/CommonWorkflowLanguage.yml")
+;; It automatically enables on-the-fly syntax checker for CWL.
+
+;;; Code:
+
+(require 'flycheck)
+
+(flycheck-def-option-var flycheck-cwl-schema-path "/please/set/path/to/v1.0/CommonWorkflowLanguage.yml" cwl
+  "A path for the schema for Common Workflow Language."
+  :type 'string
+  :safe #'stringp)
+
+;; TODO: How to choose the appropriate cwlVersion?
+(flycheck-define-checker cwl
+  "A CWL syntax checker using the Schema Salad."
+  :command ("schema-salad-tool"
+            "--quiet"
+            (eval flycheck-cwl-schema-path)
+            source)
+  :error-patterns
+  ((error line-start
+          (file-name) ":" line ":" column ": " (message (one-or-more not-newline))
+          line-end))
+  :modes cwl-mode)
+
+;;;###autoload
+(defun setup-flycheck-cwl ()
+  "Set up for flycheck cwl checker."
+  (add-to-list 'flycheck-checkers 'cwl 'append)
+  (flycheck-add-next-checker 'yaml-jsyaml '(warning . cwl))
+  (flycheck-add-next-checker 'yaml-ruby   '(warning . cwl)))
+
+(provide 'flycheck-cwl)
+;;; flycheck-cwl.el ends here
